fdeaea77daea95a24fbe0ffaae83278db70554cb
rps2305 <73200332+rps2305@users.noreply.github.com>
2023-02-23 22:11:53 +0100

    SLider

--- app.vue
+++ app.vue
@@ -1,5 +1,93 @@
+<!-- https://swiperjs.com / https://swiperjs.com/element -->
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-black w-full h-full">
+
+    <Swiper
+      :height="300"
+      :modules="[SwiperAutoplay, SwiperEffectCreative]"
+      :slides-per-view="1"
+      :loop="true"
+      :effect="'creative'"
+      :autoplay="{
+        delay: 4000,
+        disableOnInteraction: false
+      }"
+      :creative-effect="{
+        prev: {
+          shadow: false,
+          translate: ['-20%', 0, -1]
+        },
+        next: {
+          translate: ['100%', 0, 0]
+        }
+      }"
+    >
+
+      <SwiperSlide>
+        <nuxt-img src="/images/plattegrond.jpg" preset="slide"></nuxt-img>
+      </SwiperSlide>
+      <SwiperSlide>
+        <nuxt-img src="/images/Lg_Metropool-RGB.jpg" preset="slide"></nuxt-img>
+      </SwiperSlide>
+      <SwiperSlide data-swiper-autoplay="10000">
+        <video autoplay muted>
+            <source src="/videos/Stravinsky_AD_v4.mp4" type="video/mp4">
+        </video>
+      </SwiperSlide>
+      <SwiperSlide v-for="event in highlights.data" :key="event.title">
+        <div class="h-screen py-6 px-6 content-evenly bg-white">
+            <nuxt-img class="rounded-lg object-cover shadow-lg" :src="`${ runtimeConfig.public.apiUrl }${event.field_image_portrait.uri.url }`"  :alt="`${ event.title }, op BAM! Festival Hengelo (Ov.)`"></nuxt-img>
+        <div class="space-y-2">
+                <div class="space-y-1 font-medium leading-6">
+                  <h3 class="text-indigo-600 font-bold uppercase">{{ event.title }}</h3>
+                  <span class="">{{ event.field_subtitle }}</span>
+                    <div class="flex space-x-4">
+                        <div class="flex">{{ event.field_dag }}</div>
+                        <div class="flex">Main stage</div>
+                        <div class="flex">21:30 uur</div>
+                        <div class="flex">{{ event.field_tags[0].name }}</div>
+                    </div>
+                </div>
+              </div>
+         </div>     
+      </SwiperSlide>
+    </Swiper>
+
   </div>
 </template>
+
+<script setup>
+ const runtimeConfig = useRuntimeConfig();
+ const { data:highlights } = await useFetch('https://cms.bamfestival.nl/jsonapi/node/event?filter[status][value]=1&filter[promote][value]=1&sort=field_dag,-field_weight,title&include=field_image_portrait,field_location,field_tags&filter[name-filter][condition][path]=field_weight&filter[name-filter][condition][operator]=IN&filter[name-filter][condition][value][1]=5&filter[name-filter][condition][value][2]=4&jsonapi_include=1')
+    
+  
+const slides = ref(Array.from({ length: 3 }, () => {
+  const r = Math.floor(Math.random() * 256)
+  const g = Math.floor(Math.random() * 256)
+  const b = Math.floor(Math.random() * 256)
+  // Figure out contrast color for font
+  const contrast = r * 0.299 + g * 0.587 + b * 0.114 > 186 ? 'black' : 'white'
+  return { bg: `rgb(${r}, ${g}, ${b})`, color: contrast }
+}))
+</script>
+
+
+
+<style>
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  
+  font-size: 4rem;
+  font-weight: bold;
+  font-family: 'Roboto', sans-serif;
+}
+.swiper-wrapper {
+  min-width: 100vh;
+  width: 100vh;
+}
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+     modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxt/image-edge',
+        'nuxt-swiper',
+        '@nuxt/content',
+              ],
+     runtimeConfig: {
+        
+                public: {
+                    apiUrl: process.env.API_BASE_URL || "https://cms.bamfestival.nl",
+                }
+            },         
+     tailwindcss: {
+        viewer: true
+      },
+     image: {
+        presets: {
+            slide: {
+              modifiers: {
+                format: 'png',
+                width: 1920,
+                fit: 'cover',
+              }
+            }
+          }
+      },
+      swiper: {
+ 
+      },
+      content: {
+       
+      },
 })
