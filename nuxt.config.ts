cde18ab1446d3d3602c3a2bbe387a11fc3e54e9a
rps2305 <73200332+rps2305@users.noreply.github.com>
2023-02-24 00:31:19 +0100

    werken de images?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
         presets: {
             slide: {
               modifiers: {
-                format: 'png',
+                format: 'jpg',
                 width: 1920,
                 fit: 'cover',
               }
